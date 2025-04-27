674702ae5558c50e0b7e0971f6b21bcea1092539
C Huddy <cargoblix@gmail.com>
2025-04-26 20:43:25 -0400

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 "true"
+
+author "CHuddy"
+description "Recoil script with many features!"
+version "1.1.1"
+
+shared_scripts {
+    "config.lua"
+}
+
+client_scripts {
+    "client/*"
+}
